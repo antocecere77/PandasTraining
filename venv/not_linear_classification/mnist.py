87879438ca7070b8a54dd06cea2b023f780ecfa7
antocecere <antocecere77@gmail.com>
2020-02-27 07:03:30 +0100

    Multilayer perceptor example

--- venv/not_linear_classification/mnist.py
+++ venv/not_linear_classification/mnist.py
@@ -4,11 +4,11 @@ import struct
 
 def load_mnist(path="/"):
     
-    train_labels_path = os.path.join(path,"train-labels-idx1-ubyte")
-    train_images_path = os.path.join(path,"train-images-idx3-ubyte")
+    train_labels_path = os.path.join(path,"train-labels.idx1-ubyte")
+    train_images_path = os.path.join(path,"train-images.idx3-ubyte")
     
-    test_labels_path = os.path.join(path,"t10k-labels-idx1-ubyte")
-    test_images_path = os.path.join(path,"t10k-images-idx3-ubyte")
+    test_labels_path = os.path.join(path,"t10k-labels.idx1-ubyte")
+    test_images_path = os.path.join(path,"t10k-images.idx3-ubyte")
     
     labels_path = [train_labels_path, test_labels_path]
     images_path = [train_images_path, test_images_path]
